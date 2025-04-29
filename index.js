022245dc9dc8f91f3a3930b126ea08e7218c0f9c
Ghfshtfh291294@git <viethungtrieuphong1994@gmail.com>
2025-04-29 10:15:33 +0700

    install icon app

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ import App from './App';
 import {name as appName} from './app.json';
 import {messaging, onMessage, setBackgroundMessageHandler} from './firebase';
 
-// onMessage(messaging, async remoteMessage => {
-//   Alert.alert(
-//     'Foreground notification',
-//     JSON.stringify(remoteMessage.notification),
-//   );
-//   console.log('Foreground notification', remoteMessage);
-//   Linking.openURL(`todolist://app/task-detail/${remoteMessage.data?.taskId}`);
-// });
+onMessage(messaging, async remoteMessage => {
+  Alert.alert(
+    'Foreground notification',
+    JSON.stringify(remoteMessage.notification),
+  );
+  console.log('Foreground notification', remoteMessage);
+  Linking.openURL(`todolist://app/task-detail/${remoteMessage.data?.taskId}`);
+});
 
 setBackgroundMessageHandler(messaging, mess => {
   Alert.alert('background');
@@ -23,12 +23,12 @@ setBackgroundMessageHandler(messaging, mess => {
   Linking.openURL(`todolist://app/task-detail/${mess.data?.taskId}`);
 });
 
-// onNotificationOpenedApp(messaging, mess => {
-//   const data = mess.data;
-//   const taskId = data.taskId;
-//   console.log('OpenedApp notification', mess);
+onNotificationOpenedApp(messaging, mess => {
+  const data = mess.data;
+  const taskId = data.taskId;
+  console.log('OpenedApp notification', mess);
 
-//   Linking.openURL(`todolist://app/task-detail/${taskId}`);
-// });
+  Linking.openURL(`todolist://app/task-detail/${taskId}`);
+});
 
 AppRegistry.registerComponent(appName, () => App);
